ac9e3af3efb237098ea7025c056ddace9b92c368
cponcax <gponcax@gmail.com>
2015-04-23 22:05:31 -0600

    Add gem cancan & authorization

--- Gemfile
+++ Gemfile
@@ -37,3 +37,5 @@ gem 'haml', '~> 4.0.5'
 gem 'simple_form', '~> 3.0.2'
 
 gem 'devise', '~> 3.4.1'
+
+gem 'cancancan', '~> 1.10'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+5.times do |i|
+  User.create(email:'alex@gmail.com', admin:"true", password:"ta851497")
+end
